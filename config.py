885c4caca7c6b88957e94e3f6e618804be25a79f
Alisher <studying2618@gmail.com>
2025-06-18 18:16:37 +0600

    Fix: use correct environment variable names

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
-API_ID = int(os.getenv("25339045"))
-API_HASH = os.getenv("839b4f1cc25bbf70d954e392cdf22ce9")
-BOT_TOKEN = os.getenv("7491697807:AAFCP8bXiYpRHDDnp2PyPcIUJEEdRhM4ZrE")
 
-# Укажи username (например, "@manager1") или user_id (например, 123456789)
-MANAGER_CONTACT = os.getenv("@alisher_cyber@alisher_cyber")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MANAGER_CONTACT = os.getenv("MANAGER_CONTACT")
